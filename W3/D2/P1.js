6a5b6b083f3e309e0ea09ee63d5caa5c9d8569b1
Shalinihr57 <shalinihr966@gmail.com>
2026-03-03 15:23:35 +0530

    Arithematic operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,14 @@
+// Arithematic Operators
+console.log("Arithematic Operators");
+const a = 2, b = 3;
+let c=0;
+console.log("a+b:",a+b); // add
+console.log("a-b:",a-b); // sub
+console.log("a*b:",a*b); // Multiplication
+console.log("a/b:",a/b); // division
+console.log("a%b:",a%b); // Modulas
+console.log("a**b:",a**b); // Exponential
+//console.log("a++:",a++); // post-increment
+//console.log("++a:",++a); // pre-increment
+//console.log("a--:",a--); // post-decrement
+//console.log("--a:",--a); // pre-decrement
