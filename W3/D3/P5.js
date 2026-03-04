92399162fba6d9357e041dd072700313acb8d629
Shalinihr57 <shalinihr966@gmail.com>
2026-03-04 13:38:30 +0530

    Basics of objects

--- W3/D3/P5.js
+++ W3/D3/P5.js
@@ -5,5 +5,21 @@ const person = {
     isStudent:false
 };
 // console.log("Person", person);
-console.log("Name:",person.name);
-console.log("age:",person["age"]);
+// console.log("Name:",person.name);
+// console.log("age:",person["age"]);
+
+//Add a new property
+person.city="Mysore";
+console.log("Person", person);
+//Modify
+person.age=31;
+//delete
+delete person.isStudent;
+console.log("Person", person);
+
+// object constructor
+const car = new Object();
+car.make = "Audi";
+car.model = "A4";
+car.year = 2026;
+console.log(car);
