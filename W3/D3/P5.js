af3ba8836323d13ef18df6e10f1a65d5a0d151ef
Shalinihr57 <shalinihr966@gmail.com>
2026-03-04 13:23:55 +0530

    Basics of objects

---
+++ W3/D3/P5.js
@@ -0,0 +1,9 @@
+// Basics of objects
+const person = {
+    name:"Shalini",
+    age:23,
+    isStudent:false
+};
+// console.log("Person", person);
+console.log("Name:",person.name);
+console.log("age:",person["age"]);
