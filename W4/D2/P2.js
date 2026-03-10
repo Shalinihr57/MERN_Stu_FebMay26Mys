bcc8af7a08e213402bcb86e0c165baedf134fe0f
Shalinihr57 <shalinihr966@gmail.com>
2026-03-10 11:34:24 +0530

    create elements-js code

---
+++ W4/D2/P2.js
@@ -0,0 +1,19 @@
+// create element() & appendChild()
+const list = document.getElementById("list");
+let counter = 1;
+
+document.getElementById("addBtn").addEventListener("click",function(){
+    const li = document.createElement("li");
+        errmessage.textContent= " ";
+    li.textContent = "Item " + counter++;
+    list.appendChild(li);
+});
+
+document.getElementById("removeBtn").addEventListener("click",function(){
+    if(list.lastElementChild){
+    list.removeChild(list.lastElementChild);
+    }
+    else{
+    errmessage.textContent= "no items to be removed"; 
+}
+});
