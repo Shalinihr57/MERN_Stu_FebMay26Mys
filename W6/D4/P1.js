1eb04870b97733bad58b22c6bda91dcfd0743bdd
Shalinihr57 <shalinihr966@gmail.com>
2026-03-26 16:05:40 +0530

    Understanding the HTTP module

---
+++ W6/D4/P1.js
@@ -0,0 +1,8 @@
+//Understanding the HTTP module
+
+//Importing built-in HTTP module
+const http = require("http");
+
+console.log("Type of http.createServer:",typeof http.createServer);
+console.log("common HTTP methods:",http.METHODS);
+
