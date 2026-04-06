410d6093922a696530f7151bf7532c219859cb2c
Shalinihr57 <shalinihr966@gmail.com>
2026-04-06 10:25:44 +0530

    This file stores the movie data used in the CLI app

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/movies.js
@@ -0,0 +1,34 @@
+// movie.js
+//This file stores the movie data used in the CLI app
+
+const movies = [
+    {
+        id:1,
+        title:"Dhurandar2",
+        showtimes:[
+            {time: "10:00 AM",seatsAvailable: 100},
+            {time: "1:00 PM",seatsAvailable: 70},
+            {time: "6:00 AM",seatsAvailable: 300},
+        ]   
+    },
+    {
+        id:2,
+        title:"LoveMoktail3",
+        showtimes:[
+            {time: "10:00 AM",seatsAvailable: 100},
+            {time: "1:00 PM",seatsAvailable: 70},
+            {time: "6:00 AM",seatsAvailable: 300},
+        ]   
+    },
+    {
+        id:3,
+        title:"Hayagreeva",
+        showtimes:[
+            {time: "10:00 AM",seatsAvailable: 100},
+            {time: "1:00 PM",seatsAvailable: 70},
+            {time: "6:00 AM",seatsAvailable: 300},
+        ]   
+    }
+];
+//Export the movie data so that other files can use it.
+Module.export = movies;
