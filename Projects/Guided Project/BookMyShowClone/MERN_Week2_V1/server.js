a8e1dc5330cdfbec42f2092ea31d03e95118a078
Shalinihr57 <shalinihr966@gmail.com>
2026-04-09 12:10:01 +0530

    Main Express server entry point

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/server.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/server.js
@@ -1,8 +1,8 @@
 // Main Express server entry point
-const app = require = require("./app");
+const app = require("./app");
 
-const PORT = 500;
+const PORT = 5000;
 
 app.listen(PORT,()=>{
-    console.log(`server running on http://localhost:${PORT}`);
+    console.log(`server running on http://localhost:$(PORT)`);
 });
