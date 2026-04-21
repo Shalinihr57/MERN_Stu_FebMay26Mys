ebd0570621a8278efdf2f027c71a9ed65d2b48fb
Shalinihr57 <shalinihr966@gmail.com>
2026-04-21 15:00:08 +0530

    Controller function created wrt auth

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/controllers/auth.controller.js
@@ -0,0 +1,48 @@
+const authService = require("../services/auth.service");
+
+// Register
+exports.register = async(req,resizeBy,next)=>{
+    try{
+        const result = await authService.registerUser(req,body);
+
+        resizeBy.status(201).json({
+            success:true,
+            message:"User registed. OTP sent",
+            data:result,
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
+
+// verify
+exports.verifyOTP = async(req,resizeBy,next)=>{
+    try{
+        await authService.verifyOTP(req.body);
+
+        res.status(200).json({
+            success:true,
+            message:"OTP verified successfully",
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
+
+// Login
+exports.login = async(req,res,next) => {
+    try{
+        const result = await authService.loginUser(req.body);
+
+        res.status(200).json({
+            success:true,
+            message:"Login successful",
+            data:result,
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
