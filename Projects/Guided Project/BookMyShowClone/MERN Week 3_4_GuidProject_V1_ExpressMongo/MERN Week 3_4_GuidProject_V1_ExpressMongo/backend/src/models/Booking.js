4e6b22707cd310fd6c7a5f5f6a0142500b5998be
Shalinihr57 <shalinihr966@gmail.com>
2026-04-23 13:30:41 +0530

    error forwarding method changed

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Booking.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Booking.js
@@ -2,22 +2,18 @@ const mongoose = require("mongoose");
 const bookingSchema = new mongoose.Schema({
     userId:{
         type:mongoose.Schema.Types.ObjectId,
-        ref:"user",
+        ref:"User",
         required:true,
         index:true,
     },
     showId:{
         type:mongoose.Schema.Types.ObjectId,
-        ref:"Show",
+        ref:"show",
         required:true,
-        index:true,
+        index:true
     },
     seats:{
-        type:[String],
-        required:true,
-    },
-    totalSeats:{
-        type:Number,
+        type:String,
         required:true,
     },
     status:{
@@ -35,17 +31,20 @@ const bookingSchema = new mongoose.Schema({
     timestamps:true,
 });
 
-// Add validation 
-bookingSchema.pre("save",function(next){
-    if(this.seats.length === 0){
-        return next(new Error("At least one seat must be selected"));
+// Add Validation
+bookingSchema.pre("save",function () {
+    if (this.seats.length === 0) {
+        // return next(new Error("At Least One Seat Must Be Selected"));
+         throw new Error("At Least One Seat Must Be Selected");
     }
-    if(this.totalSeats!=this.seats.length){
-        return next(new Error("Seat count mismatch"));
+    if (this.totalSeats!=this.seats.length) {
+        // return next(new Error("Seat Count Mismatch"));
+        throw new Error("Seat Count Mismatch");
     }
-    next();
 });
-// Compound index
+
+// Compound Index
+
 bookingSchema.index({userId:1,showId:1});
 
 module.exports = mongoose.model("Booking",bookingSchema);
