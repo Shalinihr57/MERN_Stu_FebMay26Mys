eec3320399c05f19b40c71ab3de454d151fdac2a
Shalinihr57 <shalinihr966@gmail.com>
2026-04-23 13:19:28 +0530

    Created CRUD operation functions with req & res and exported it

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/controllers/show.controller.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/controllers/show.controller.js
@@ -1,12 +1,12 @@
 const showService = require("../services/show.service");
 
-// create show: Admin
-exports.createShow = async(req,res,next) => {
+// Create Show : Admin
+exports.createShow = async (req,res,next) => {
     try{
         const show = await showService.createShow(req.body);
         res.status(201).json({
             success:true,
-            message:"show created successfully",
+            message:"Show created successfully",
             data:show,
         });
     }
@@ -15,13 +15,13 @@ exports.createShow = async(req,res,next) => {
     }
 };
 
-// Get shows
-exports.getShows = async(req,res,next) => {
+//Get shows
+exports.getShows = async (req,res,next) => {
     try{
         const shows = await showService.getShows(req.query);
         res.status(200).json({
             success:true,
-            message:"shows created successfully",
+            message:"Shows fetched successfully",
             data:shows,
         });
     }
@@ -29,14 +29,13 @@ exports.getShows = async(req,res,next) => {
         next(error);
     }
 };
-
-// Get single show
-exports.getShowById = async(req,res,next) => {
+//Get Single show
+exports.getShowById = async (req,res,next) => {
     try{
         const show = await showService.getShowById(req.params.id);
         res.status(200).json({
             success:true,
-            message:"show fetched successfully",
+            message:"Show fetched successfully",
             data:show,
         });
     }
@@ -45,13 +44,13 @@ exports.getShowById = async(req,res,next) => {
     }
 };
 
-// Update show
-exports.updateShow = async(req,res,next) => {
+//update show - admin
+exports.updateShow = async (req,res,next) => {
     try{
         const show = await showService.updateShow(req.params.id,req.body);
         res.status(200).json({
             success:true,
-            message:"show updated successfully",
+            message:"Show updated successfully",
             data:show,
         });
     }
@@ -60,13 +59,13 @@ exports.updateShow = async(req,res,next) => {
     }
 };
 
-// Delete show - admin
-exports.deleteShow = async(req,res,next) => {
+//delete show - admin
+exports.deleteShow = async (req,res,next) => {
     try{
         await showService.deleteShow(req.params.id);
         res.status(200).json({
             success:true,
-            message:"show deleted successfully",
+            message:"Show deleted successfully",
         });
     }
     catch(error){
