970a8834754003253f26228521c239cdf090e7df
Shalinihr57 <shalinihr966@gmail.com>
2026-04-29 10:36:32 +0530

    Prop drilling component added

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import { PropBasics } from "./components/P1";
 import { PropDestructuring } from "./components/P2";
+import {PropChildren} from "./components/P3";
+import {PropDrillingDemo} from "./components/P4"
 function App() {
   return (
     <>
       {/* <PropBasics /> */}
-      <PropDestructuring />
+      {/* <PropDestructuring /> */}
+      {/* <PropChildren/> */}
+      <PropDrillingDemo/>
     </>
   )
 }
