128a7b751996d5460f42afa9883b65fdc469c279
Shalinihr57 <shalinihr966@gmail.com>
2026-04-30 08:57:55 +0530

    Higher order component(HOC) example

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,21 +1,32 @@
 import './App.css'
 import { PropBasics } from "./components/P1";
 import { PropDestructuring } from "./components/P2";
-import {PropChildren} from "./components/P3";
-import {PropDrillingDemo} from "./components/P4";
-import {SharedStateParent} from "./components/P5";
-import {CompositionDemo} from "./components/P6";
-import {ObjectsAndArrayProps} from "./components/P7";
+import { PropsChildren } from "./components/P3";
+import { PropDrillingDemo } from "./components/P4";
+import { SharedStateParent } from "./components/P5";
+import { CompositionDemo } from "./components/P6";
+import { ObjectsAndArrayProps } from "./components/P7";
+import { PropsImmutability } from "./components/P8";
+import { PropsState } from "./components/P9";
+import { FunctionProps } from "./components/P10";
+import { PropTypesDemo } from "./components/P11";
+import { HOCExample } from "./components/P12";
+
 function App() {
   return (
     <>
       {/* <PropBasics /> */}
       {/* <PropDestructuring /> */}
-      {/* <PropChildren/> */}
-      {/* <PropDrillingDemo/> */}
+      {/* <PropsChildren /> */}
+      {/* <PropDrillingDemo /> */}
       {/* <SharedStateParent/> */}
-      {/* <CompositionDemo/> */}
-      <ObjectsAndArrayProps/>
+      {/* <CompositionDemo /> */}
+      {/* <ObjectsAndArrayProps /> */}
+      {/* <PropsImmutability /> */}
+      {/* <PropsState/> */}
+      {/* <FunctionProps /> */}
+      {/* <PropTypesDemo /> */}
+      <HOCExample />
     </>
   )
 }
