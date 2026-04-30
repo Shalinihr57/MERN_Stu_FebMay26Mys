910cb3c49e9858840400294c45b992381ae39f29
shalinihr57 <shalinihr966@gmail.com>
2026-04-30 10:03:06 +0530

    Custom error class created for error handling

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/utils/customError.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/utils/customError.js
@@ -3,7 +3,6 @@ class CustomError extends Error{
     constructor(message,statusCode){
         super(message);
         this.statusCode = statusCode;
-        Error.captureStackTrace(this, this.constructor);
     }
 }
 
