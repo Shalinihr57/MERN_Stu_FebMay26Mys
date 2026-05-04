1f636b5b7965585ec1b382d11d20659cd4ac7e94
shalinihr57 <shalinihr966@gmail.com>
2026-05-04 11:44:19 +0530

    Update objects and Arrays

---
+++ W10/D1/state-hooks-app/src/components/P2.jsx
@@ -0,0 +1,24 @@
+// Update objects and Arrays
+import { useState } from "react";
+
+export function UpdatingObjectsArrayState(){
+    // user state
+    const [user, setUser] = useState({
+        name:'Shalini',
+        skill:'React'
+    });
+
+    const updateSkill = ()=>{
+        setUser({
+            ...user, // copies all existing properties(name,skill)
+            skill:'Advanced React'
+        });
+    };
+    return(
+        <>
+        <h2>Updating objects state</h2>
+        <p>{user.name} - {user.skill}</p>
+        <button onClick={updateSkill}>Update Skill</button>
+        </>
+    )
+}
