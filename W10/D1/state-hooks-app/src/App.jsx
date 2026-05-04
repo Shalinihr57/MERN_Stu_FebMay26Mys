9f393ca295f8a5d85ead5d3a5976fe35a8255aef
shalinihr57 <shalinihr966@gmail.com>
2026-05-04 10:50:02 +0530

    added W10/D1 state & hooks app

---
+++ W10/D1/state-hooks-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { UseStateBasics } from './components/P1'
+
+function App() {
+
+  return (
+    <>
+ <UseStateBasics/>
+    </>
+  )
+}
+
+export default App
