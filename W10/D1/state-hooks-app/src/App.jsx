eacb415c4e9a2895193c7c74796c0b68e493242c
shalinihr57 <shalinihr966@gmail.com>
2026-05-04 11:44:47 +0530

    Update objects and Arrays

--- W10/D1/state-hooks-app/src/App.jsx
+++ W10/D1/state-hooks-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { UseStateBasics } from './components/P1'
+import {UpdatingObjectsArrayState} from'./components/P2'
 
 function App() {
 
   return (
     <>
- <UseStateBasics/>
+ {/* <UseStateBasics/> */}
+ <UpdatingObjectsArrayState/>
     </>
   )
 }
