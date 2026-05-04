9f06d33bd11ef17b5bec5078bd9a0f1e5e4f57ad
shalinihr57 <shalinihr966@gmail.com>
2026-05-04 12:00:49 +0530

    Event handling with state

---
+++ W10/D1/state-hooks-app/src/components/P3.jsx
@@ -0,0 +1,26 @@
+// Event handling with state
+import { useState } from "react";
+
+// In react: 
+// events use camelCase naming, eg: onClick, onChange 
+export function EventHandlingState(){
+    const [name, setName] = useState('');
+
+    const handleChange = (e)=>{
+        setName(e.target.value);
+    };
+
+    const handleClick = ()=>{
+        alert(`Hello, ${name || 'Guest'}`)
+    };
+    return(
+        <>
+        <h2>Event Handling with state</h2>
+        <input type="text" val={name}
+        onChange={handleChange}
+        placeholder="Enter your name" />
+        <button onClick={handleClick}>Greet user</button>
+        <p>Current input: {name}</p>
+        </>
+    )
+}
