eab44f0f3aa170a3b5cd3a18e1e57a1298f95865
shalinihr57 <shalinihr966@gmail.com>
2026-05-25 11:00:51 +0530

    Managing API state with useEffect

---
+++ W11/D3/my-app/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import {ManagingApiState} from "./components/P1.jsx";
+import {UseEffectLifecycle} from "./components/P2.jsx";
+import {AxiosLifecycle} from "./components/P3.jsx";
+import {CRUDOperation} from "./components/P4.jsx";
+function App() {
+
+  return (
+    <>
+      {/* <ManagingApiState/> */}
+      {/* <UseEffectLifecycle/> */}
+      {/* <AxiosLifecycle/> */}
+      <CRUDOperation/>
+    </>
+  )
+}
+
+export default App
